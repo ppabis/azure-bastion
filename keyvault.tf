f999e01b9c230eac563c5028087a7e4570f9155e
ppabis <piotr@pabis.eu>
2025-05-27 20:53:14 +0200

    Store the SSH private key and allow Bastion to use it

--- keyvault.tf
+++ keyvault.tf
@@ -21,3 +21,21 @@ resource "azurerm_key_vault_access_policy" "bastion_keyvault_access_policy" {
   object_id          = data.azurerm_client_config.current.object_id
   secret_permissions = ["Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"]
 }
+
+data "azuread_service_principal" "bastion_service_principal" {
+  display_name = "Azure Bastion"
+}
+
+resource "azurerm_key_vault_access_policy" "bastion_service_principal_access_policy" {
+  key_vault_id       = azurerm_key_vault.bastion_keyvault.id
+  tenant_id          = data.azurerm_client_config.current.tenant_id
+  object_id          = data.azuread_service_principal.bastion_service_principal.object_id
+  secret_permissions = ["Get", "List"]
+}
+
+resource "azurerm_key_vault_secret" "bastion_key" {
+  name         = "vm1-key"
+  value        = tls_private_key.ssh_key.private_key_pem
+  key_vault_id = azurerm_key_vault.bastion_keyvault.id
+  depends_on   = [ azurerm_key_vault_access_policy.bastion_keyvault_access_policy ]
+}
